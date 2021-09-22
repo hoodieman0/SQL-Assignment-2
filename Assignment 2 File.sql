3a06b69bf8d7840a6c4c83c9b78d67eed76ab52a
hoodieman0 <hoodieman8@gmail.com>
2021-09-22 14:00:03 -0400

    Part 1c
    Completion of Part 1c

--- Assignment 2 File.sql
+++ Assignment 2 File.sql
@@ -9,13 +9,15 @@ SELECT Fname, Minit, Lname FROM employee WHERE salary > 27000;
 
 SELECT Fname, Lname, Address FROM employee WHERE Ssn = ANY (SELECT Essn FROM dependent WHERE Relationship = "Son");
 
+/* Part 1c */
+SELECT concat(e.Fname, ' ', e.Minit, '. ', e.Lname) AS Employee,
+	IFNULL(concat(m.Fname, ' ', m.Minit, '. ', m.Lname), 'Top Supervisor') AS Supervisor
+    FROM employee e
+    LEFT JOIN employee m ON
+    m.Ssn = e.Super_ssn
+    ORDER BY e.Lname;
 
 /*
-b. List the first name, last name, and address of all employees who have a son as a dependent but do not have a daughter as a dependent.
-
-c. List the full name (first name, middle initial, and last name) of each employee in a single column named “Employee”, 
-	sorting the employees in alphabetical order by last name. If the employee has a supervisor, list the full name of the supervisor in a single column named “Supervisor”. 
-
 d. List the name and total working hours for all projects based in Houston that have more than two employees working on them. e. List the SSN, first name, middle initial, 
 	and last name of all employees who work in the department that has the employee with the highest salary among all employees. 
 

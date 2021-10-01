60ccea2e102fb59c5b24070a053782d1e57a82d7
James Mok <hoodieman8@gmail.com>
2021-10-01 10:44:31 -0400

    Transfer version
    I want to work on my laptop

--- Assignment 2 File.sql
+++ Assignment 2 File.sql
@@ -28,10 +28,24 @@ SELECT ssn, fname, minit, lname FROM employee
 				WHERE salary = (SELECT max(salary) FROM employee)
                 );
 
-/*
-e. List the SSN, first name, middle initial, and last name of all employees who work in the department that has the employee with 
-	the highest salary among all employees. 
+/* Part 1f INCOMPLETE*/
+SELECT pname, dname FROM project p INNER JOIN department d ON p.dnum = d.dnumber AND d.mgr_ssn = ANY ((SELECT essn FROM works_on GROUP BY essn HAVING hours >=20));
+SELECT mgr_ssn, sum(w.hours) FROM department d LEFT JOIN works_on w ON d.mgr_ssn = w.essn GROUP BY mgr_ssn HAVING sum(w.hours) > 20;
 
+SELECT essn FROM works_on GROUP BY essn HAVING sum(hours) >20;
+SELECT pname, pnumber, dname, dnum FROM project p JOIN department d ON p.dnum = d.dnumber AND d.mgr_ssn = ANY (SELECT pno FROM works_on GROUP BY pno HAVING hours >= 20);
+SELECT essn, hours FROM works_on Group by essn having hours >= 20;
+
+/* Part 1g */
+SELECT dname FROM department LEFT JOIN employee ON ;
+SELECT fname, dno, dependent_name FROM employee LEFT JOIN dependent ON ;
+
+/* Part 1h */
+SELECT plocation FROM project WHERE department.dnumber = (SELECT min(SELECT hours FROM works_on GROUP BY essn ) FROM works_on);
+SELECT * FROM department JOIN works_on JOIN project;
+SELECT * FROM works_on WHERE hours IN (SELECT pno, hours from works_on GROUP BY pno) ;
+
+/*
 f. List the project name and the department that manages that project for all projects that have supervisors working on them for at least 20 hours. 
 
 g. List the name of the department whose employees have the greatest total of dependents. 
